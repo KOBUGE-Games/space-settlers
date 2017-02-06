5875a30da72d0da1a0d70c54371ff11d5fa55f7d
Alket Rexhepi <alketii@gmail.com>
2017-02-06 17:06:42 +0100

    some icons and hud

--- CREDITS.md
+++ CREDITS.md
@@ -9,6 +9,7 @@
 ### Graphics
 - [Space Background by Rawdanitsu](http://opengameart.org/content/space-backgrounds-7)
 - [Dices by JamesWhite](http://opengameart.org/content/dice-4)
+- [Some of icons by HorrorPen ](http://opengameart.org/content/random-rpg-icons-part-1)
 
 
 ### Special Thanks
--- scripts/main.gd
+++ scripts/main.gd
@@ -1,5 +1,44 @@
 extends Node2D
 
+const TOP = 0
+const RIGHT = 1
+const BOTTOM = 2
+const LEFT = 3
+
+const SCROLLING_SPACE = 20
+const SCROLLING_STEP = 100
+
+var scrolling = -1
+
+onready var camera = get_node("camera_main")
+
 func _ready():
 	randomize()
+	set_process_input(true)
+	set_fixed_process(true)
 	#OS.set_window_maximized(true)
+
+func _input(event):
+	if event.type == InputEvent.MOUSE_MOTION:
+		if event.pos.x >= 0 and event.pos.x < SCROLLING_SPACE:
+			scrolling = LEFT
+		elif event.pos.x > OS.get_window_size().x - SCROLLING_SPACE and event.pos.x < OS.get_window_size().x:
+			scrolling = RIGHT
+		elif event.pos.y > 0 and event.pos.y < SCROLLING_SPACE:
+			scrolling = TOP
+		elif event.pos.y > OS.get_window_size().y - SCROLLING_SPACE and event.pos.y < OS.get_window_size().y:
+			scrolling = BOTTOM
+		else:
+			scrolling = -1
+			
+func _fixed_process(delta):
+	if scrolling == LEFT:
+		camera.set_offset(camera.get_offset() - Vector2(SCROLLING_STEP*delta,0))
+	elif scrolling == RIGHT:
+		camera.set_offset(camera.get_offset() + Vector2(SCROLLING_STEP*delta,0))
+	elif scrolling == TOP:
+		camera.set_offset(camera.get_offset() - Vector2(0,SCROLLING_STEP*delta))
+	elif scrolling == BOTTOM:
+		camera.set_offset(camera.get_offset() + Vector2(0,SCROLLING_STEP*delta))
+
+
