288573bf710ca84bc2f2b1286d20285667fdb78f
Alket Rexhepi <alketii@gmail.com>
2017-02-13 19:37:33 +0100

    menu changes

---
+++ global.gd
@@ -0,0 +1,6 @@
+extends Node
+
+onready var main_menu = get_node("/root/main")
+
+func _ready():
+	pass
--- gui/button.gd
+++ gui/button.gd
@@ -9,3 +9,9 @@ func _ready():
 func _on_button_pressed():
 	if name == "quit":
 		get_tree().quit()
+	else:
+		for window in global.main_menu.get_node("window/windows").get_children():
+			window.hide()
+		
+	if name == "host" or name == "join" or name == "settings" or name == "help":
+		global.main_menu.get_node("window/windows/"+name).show()
